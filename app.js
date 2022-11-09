cb12f3896bac4694b95eb89475edf782288707d7
afurkanbicak <afurkanbicak@gmail.com>
2022-11-09 15:24:22 +0300

    mongoDb connection made

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const { response } = require("express")
 const express = require("express")
 const app = express()
+
+const mongoose = require("mongoose")
 
 const products = require("./routes/products")
 const home = require("./routes/home")
@@ -10,9 +11,47 @@ app.use(express.json())
 app.use("/api/products", products);
 app.use("/", home)
 
-app.get("/", (req, res) => {
-    res.send("Popüler ürünler")
+(async () => {
+    try{
+        mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.tpaftbe.mongodb.net/${db}?retryWrites=true&w=majority`)
+        console.log("mongodb bağlantısı kuruldu.")
+    } catch(err) {
+        console.log(err)
+    }
+})();
+
+const productSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String,
+    imageUrl: String,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    isActive: Boolean
 })
+
+const Product = mongoose.model("Product", productSchema) // model
+const prd = new Product({
+    name: "İphone 14",
+    price: 1600,
+    description: "good phone",
+    imageUrl: "1.jpeg",
+    isActive: true
+})
+
+async function saveProduct() {
+    try {
+        const result = await prd.save()
+        console.log("product", result)
+    }
+    catch (err) {
+        console.log("Error", err)
+    }
+}
+
+saveProduct();
 
 app.listen(3001, () => {
     console.log("Listening on port 3001")
